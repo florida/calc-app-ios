6b32576406e6a73963fdee7af4f0df86579ea3ca
florida <florida.elago@gmail.com>
2015-05-25 20:48:17 -0700

    add numbers and operators

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTyping = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if userIsTyping {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsTyping = true
+        }
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        switch operation {
+        case "×":
+        case "÷":
+        case "+": 
+        case "−":
+            default: break
+        }
     }
 
-
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        userIsTyping = false;
+        operandStack.append(displayValue);
+    }
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+    }
 }
 
